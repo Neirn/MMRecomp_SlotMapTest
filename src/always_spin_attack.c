d69d9ff0f716e557c69ca2c9e479b71157e62b4b
Neirn <neirny@gmail.com>
2025-06-03 16:07:16 -0700

    This always crashes?

--- src/always_spin_attack.c
+++ src/always_spin_attack.c
@@ -2,16 +2,25 @@
 #include "global.h"
 #include "recomputils.h"
 #include "recompconfig.h"
+#include "recompdata.h"
+
+typedef struct {
+    unsigned a;
+} TestStruct;
 
 // Patches a function in the base game that's used to check if the player should quickspin.
 RECOMP_PATCH s32 Player_CanSpinAttack(Player* this) {
-    recomp_printf("enum_option: %d\n", recomp_get_config_u32("enum_option"));
-    recomp_printf("number_option: %d\n", recomp_get_config_double("number_option"));
-    char* string_option = recomp_get_config_string("string_option");
-    if (string_option != NULL) {
-        recomp_printf("string_option: %s\n", string_option);
-        recomp_free_config_string(string_option);
-    }
+    recomp_printf("TESTING MEMORY SLOTMAP\n");
+    MemorySlotmapHandle h = recomputil_create_memory_slotmap(sizeof(TestStruct));
+    recomp_printf("MEMORY SLOTMAP CREATED\n");
+
+    collection_key_t key = recomputil_memory_slotmap_create(h); // <--- CRASHES ON THIS LINE
+    recomp_printf("MEMORY SLOTMAP NEW ELEMENT ADDED\n");
+
+    recomputil_destroy_memory_slotmap(h);
+    recomp_printf("MEMORY SLOTMAP DESTROYED\n");
+
+    recomp_printf("MEMORY SLOTMAP SUCCESS\n");
 
     // Always spin attack.
     return true;
